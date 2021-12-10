b95769fb70b9171eda83d0924942d83a21ba6944
lorivalSouza <lorivalnew@gmail.com>
2021-12-10 14:22:51 -0300

    Finish and send to evaluation fix

---
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,57 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.service.exceptions.ResourceNotFoundException;
+
+@Service
+public class MovieService {
+	
+	@Autowired
+	private MovieRepository repository;
+	
+	@Autowired
+	private GenreRepository genreRepository;
+	
+	
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAllPaged(Long genreId, Pageable pageable){
+		Genre genre = (genreId == 0) ? null : genreRepository.getOne(genreId);
+		
+		Page<Movie> page = repository.find(genre, pageable);			
+		repository.findMoviesWithgenre(page.getContent());
+		return page.map(x -> new MovieDTO(x));
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Optional<Movie> obj = repository.findById(id);
+		Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entiy not found"));
+		
+		return new MovieDTO(entity, entity.getReviews());
+	}
+	
+	@Transactional(readOnly = true)
+	public List<ReviewDTO> findAllReviewByMovieId(Long id) {
+	List<Review> reviews = repository.findAllReviewByMovieId(id);
+	return reviews.stream().map(x -> new ReviewDTO(x)).collect(Collectors.toList());
+	} 
+
+
+}
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -5,7 +5,6 @@ import { getAuthData } from "./storage";
 
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ??
-  'http://192.168.0.183:8080' ??
   'http://localhost:8080';
 
   const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'myclientid';
